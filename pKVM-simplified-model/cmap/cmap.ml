2c66d468f7ab069cf82c7f794dfecbff5fb3cea8
Thomas Fourier <fourier.thomas@gmail.com>
2024-05-29 11:51:07 +0000

    OFFLINE: PORT: SM: performance: cmap packetize by page

--- pKVM-simplified-model/cmap/cmap.ml
+++ pKVM-simplified-model/cmap/cmap.ml
@@ -1,9 +1,25 @@
 module ZMap = Map.Make (Z)
 
 type 'a cache = (Z.t * 'a option array) list
-type 'a t = { c : 'a cache; map : 'a ZMap.t }
+type 'a t = { c : 'a cache; map : 'a option array ZMap.t }
 
 let empty = { c = []; map = ZMap.empty }
-let insert x va ma = { c = []; map = ZMap.add x va ma.map }
-let lookup k ma = try Some (ZMap.find k ma.map) with Not_found -> None
+let map_bits x = Z.shift_right x 12
+let array_bits x = Z.(to_int (shift_right (logand x (of_int 0xfff)) 3))
+
+let rec insert x va ma =
+  try
+    let array = ZMap.find (map_bits x) ma.map in
+    Array.set array (array_bits x) (Some va);
+    ma
+  with Not_found ->
+    insert x va
+      {
+        c = [];
+        map = ZMap.add (Z.shift_right x 12) (Array.make 512 None) ma.map;
+      }
+
+let lookup x ma =
+  try (ZMap.find (map_bits x) ma.map).(array_bits x) with Not_found -> None
+
 let () = ignore empty.c
