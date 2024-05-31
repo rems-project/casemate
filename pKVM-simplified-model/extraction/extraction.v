8bed4fc9c683724f84ce12df738f89e1ffd21dc2
Thomas Fourier <thomas.fourier@polytechnique.edu>
2024-05-31 18:13:34 +0100

    OFFLINE: PORT: SM: separate Z from Coq and from Ocaml in extraction

--- pKVM-simplified-model/extraction/extraction.v
+++ pKVM-simplified-model/extraction/extraction.v
@@ -11,24 +11,22 @@ Extract Inlined Constant n512 => "512".
 
 Extract Inlined Constant BV64 => "".
 Extract Inlined Constant Z_to_bv_checked => "(fun _ _ -> ())".
-Extract Inlined Constant b0 => "(Big_int_Z.big_int_of_int 0)".
-Extract Inlined Constant b1 => "(Big_int_Z.big_int_of_int 1)".
-Extract Inlined Constant b2 => "(Big_int_Z.big_int_of_int 2)".
-Extract Inlined Constant b8 => "(Big_int_Z.big_int_of_int 8)".
-Extract Inlined Constant b12 => "(Big_int_Z.big_int_of_int 12)".
-Extract Inlined Constant b16 => "(Big_int_Z.big_int_of_int 16)".
-Extract Inlined Constant b47 => "(Big_int_Z.big_int_of_int 47)".
-Extract Inlined Constant b63 => "(Big_int_Z.big_int_of_int 63)".
-Extract Inlined Constant b512 => "(Big_int_Z.big_int_of_int 512)".
-Extract Inlined Constant b1023 => "(Big_int_Z.big_int_of_int 1023)".
+Extract Inlined Constant b0 => "(Za.of_int 0)".
+Extract Inlined Constant b1 => "(Za.of_int 1)".
+Extract Inlined Constant b2 => "(Za.of_int 2)".
+Extract Inlined Constant b8 => "(Za.of_int 8)".
+Extract Inlined Constant b12 => "(Za.of_int 12)".
+Extract Inlined Constant b16 => "(Za.of_int 16)".
+Extract Inlined Constant b47 => "(Za.of_int 47)".
+Extract Inlined Constant b63 => "(Za.of_int 63)".
+Extract Inlined Constant b512 => "(Za.of_int 512)".
+Extract Inlined Constant b1023 => "(Za.of_int 1023)".
 
-Extract Inlined Constant bv_add => "(fun _ a b -> Z.add a b)".
-Extract Inlined Constant bv_sub => "(fun _ a b -> Z.sub a b)".
-Extract Inlined Constant bv_mul => "(fun _ a b -> Z.mul a b)".
-Extract Inlined Constant bv_and => "(fun _ a b -> Big_int_Z.and_big_int a b)".
-Extract Inlined Constant bv_mul_Z => "(fun _ a b -> Z.mul a b)".
-
-
+Extract Inlined Constant bv_add => "(fun _ a b -> Za.add a b)".
+Extract Inlined Constant bv_sub => "(fun _ a b -> Za.sub a b)".
+Extract Inlined Constant bv_mul => "(fun _ a b -> Za.mul a b)".
+Extract Inlined Constant bv_and => "(fun _ a b -> Za.logand a b)".
+Extract Inlined Constant bv_mul_Z => "(fun _ a b -> Za.mul a b)".
 
 Extract Inlined Constant ghost_simplified_model_state => "sm_location Cmap.t".
 Extract Inlined Constant ghost_simplified_model_zallocd => "unit Cmap.t".
@@ -50,5 +48,4 @@ Set Extraction Flag 2031.
 Set Extraction Output Directory ".".
 
 Set Warnings "-extraction-opaque-accessed".
-Extraction "coq_executable_sm.ml"
-  all_steps memory_0 step.
+Extraction "coq_executable_sm.ml" all_steps memory_0 step.
