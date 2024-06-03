e252a9372b2c95c9a9a66ce0f2c28c0e6100e72a
Thomas Fourier <thomas.fourier@polytechnique.edu>
2024-06-03 15:00:09 +0100

    OFFLINE: PORT: SM: make extraction more readable

--- pKVM-simplified-model/extraction/extraction.v
+++ pKVM-simplified-model/extraction/extraction.v
@@ -23,16 +23,16 @@ Extract Inlined Constant b63 => "(Za.of_int 63)".
 Extract Inlined Constant b512 => "(Za.of_int 512)".
 Extract Inlined Constant b1023 => "(Za.of_int 1023)".
 
-Extract Inlined Constant bv_add => "(fun _ a b -> Za.add a b)".
-Extract Inlined Constant bv_sub => "(fun _ a b -> Za.sub a b)".
-Extract Inlined Constant bv_mul => "(fun _ a b -> Za.mul a b)".
-Extract Inlined Constant bv_and => "(fun _ a b -> Za.logand a b)".
-Extract Inlined Constant bv_mul_Z => "(fun _ a b -> Za.mul a b)".
+Extract Inlined Constant bv_add => "(fun _ -> Za.add)".
+Extract Inlined Constant bv_sub => "(fun _ -> Za.sub)".
+Extract Inlined Constant bv_mul => "(fun _ -> Za.mul)".
+Extract Inlined Constant bv_and => "(fun _ -> Za.logand)".
+Extract Inlined Constant bv_mul_Z => "(fun _ -> Za.mul)".
 
-Extract Inlined Constant ghost_simplified_model_state => "sm_location Cmap.t".
+Extract Inlined Constant ghost_simplified_model_state => "(sm_location Cmap.t)".
 Extract Inlined Constant cmap_empty => "(Cmap.empty ())".
-Extract Inlined Constant cmap_lookup => "(fun g m -> Cmap.lookup g m)".
-Extract Inlined Constant cmap_insert => "(fun k a m -> Cmap.insert k a m)".
+Extract Inlined Constant cmap_lookup => "Cmap.lookup".
+Extract Inlined Constant cmap_insert => "Cmap.insert".
 
 
 Extract Inductive result => "Stdlib.result" [ "Ok" "Error" ].
