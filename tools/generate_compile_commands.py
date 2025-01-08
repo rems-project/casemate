88cea24c73f8d768ee748449b3c9802c5156a50e
Ben Simner <benjsimner@gmail.com>
2025-01-08 15:08:32 +0000

    (clangd) use casemate root as working dir for all clangd commands

--- tools/generate_compile_commands.py
+++ tools/generate_compile_commands.py
@@ -8,21 +8,22 @@ import pathlib
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("dir", type=pathlib.Path)
+parser.add_argument("srcdir", type=pathlib.Path)
+parser.add_argument("workdir", type=pathlib.Path)
 args = parser.parse_args()
 
 compile_commands = []
 
-for f in args.dir.rglob("*.c"):
+for f in args.srcdir.rglob("*.c"):
     cmd_path = f.with_suffix(".o.cmd")
     if cmd_path.exists():
         compile_commands.append(
             {
                 "command": cmd_path.read_text(),
-                "directory": str(args.dir),
-                "file": str(f),
+                "directory": str(args.workdir.absolute()),
+                "file": str(f.absolute()),
             }
         )
 
-with open(args.dir / "compile_commands.json", "w") as f:
+with open(args.srcdir / "compile_commands.json", "w") as f:
     json.dump(compile_commands, f, indent=4)
