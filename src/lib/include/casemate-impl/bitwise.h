b1c8ffdc704dd538239471fe5a78b461c496726d
Ben Simner <benjsimner@gmail.com>
2025-04-13 14:21:43 +0000

    (lib/bitwise) fix: confused bitmask
    When compiled separately the GENMASK macro meant something different.
    
    This clarifies that GENMASK(hi, lo) is always a `hi - lo + 1`-length
    string of 1s starting at bit `lo`.

--- src/lib/include/casemate-impl/bitwise.h
+++ src/lib/include/casemate-impl/bitwise.h
@@ -3,7 +3,7 @@
 
 #ifndef __KVM_NVHE_HYPERVISOR__
 #define BIT(I) (1UL << (I))
-#define GENMASK(L, S) (((1UL << (L)) - 1UL) << (S))
+#define GENMASK(HI, LO) (((1UL << ((HI) - (LO) + 1)) - 1UL) << (LO))
 #else
 #include <linux/bits.h>
 #endif
