1b29663c8817360c1f1e996c0df4fe8a5995de4b
Ben Simner <benjsimner@gmail.com>
2025-04-13 14:22:57 +0000

    (src/bitwise) rename GENMASK -> BITMASK
    Evolve away from Linux naming scheme and be clearer.

--- src/lib/include/casemate-impl/bitwise.h
+++ src/lib/include/casemate-impl/bitwise.h
@@ -3,9 +3,10 @@
 
 #ifndef __KVM_NVHE_HYPERVISOR__
 #define BIT(I) (1UL << (I))
-#define GENMASK(HI, LO) (((1UL << ((HI) - (LO) + 1)) - 1UL) << (LO))
+#define BITMASK(HI, LO) (((1UL << ((HI) - (LO) + 1)) - 1UL) << (LO))
 #else
 #include <linux/bits.h>
+#define BITMASK(HI, LO) GENMASK(HI, LO)
 #endif
 
 #endif /* BITWISE_H */
