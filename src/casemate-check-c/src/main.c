14622cb99e6dd9a2cb048d443f33be0c2a0d2099
Ben Simner <benjsimner@gmail.com>
2025-04-15 14:14:51 +0000

    (clang-format) deploy fmt on casemate-check-c

--- src/casemate-check-c/src/main.c
+++ src/casemate-check-c/src/main.c
@@ -18,22 +18,22 @@ int main(int argc, char **argv)
 	struct casemate_model_step step;
 
 	void *parser = make_parser(f, &step);
-	while (!parser_at_EOF(parser) && !parser_at_exclamation(parser)) {
+	while (! parser_at_EOF(parser) && ! parser_at_exclamation(parser)) {
 		parse_record(parser);
 		casemate_model_step(step);
 	}
 
 	if (parser_at_exclamation(parser)) {
 		/* logfile claimed there was an error here ... */
-		fprintf(stderr, "! casemate-check: mismatch between success status, logfile had violation but none detected.\n");
+		fprintf(stderr,
+			"! casemate-check: mismatch between success status, logfile had violation but none detected.\n");
 		ret = 1;
 	} else {
 		/* at EOF:
 		 * no more transitions, steps had no errors, all good! !*/
-		 fprintf(stderr, "casemate-check: log checked successfully.\n");
-		 ret = 0;
+		fprintf(stderr, "casemate-check: log checked successfully.\n");
+		ret = 0;
 	}
-
 
 	free(parser);
 	free(st);
