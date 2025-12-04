70dac78e3212e572a904d18ef0188b50e1fe80a5
Ben Simner <ben.simner@cl.cam.ac.uk>
2025-12-04 21:12:09 +0000

    (lib) add TLBI ALLE1, ALLE2, ALLE2IS

--- src/lib/src/model.c
+++ src/lib/src/model.c
@@ -184,7 +184,10 @@ enum sm_tlbi_op_method_kind decode_tlbi_method_kind(enum tlbi_kind k)
 	case TLBI_ipas2e1is:
 		return TLBI_OP_BY_INPUT_ADDR;
 
+	case TLBI_alle1:
 	case TLBI_alle1is:
+	case TLBI_alle2:
+	case TLBI_alle2is:
 		return TLBI_OP_BY_ALL;
 
 	default:
@@ -201,10 +204,13 @@ bool decode_tlbi_shootdown_kind(enum tlbi_kind k)
 	case TLBI_vae2is:
 	case TLBI_ipas2e1is:
 	case TLBI_alle1is:
+	case TLBI_alle2is:
 		return true;
 
 	case TLBI_vmalls12e1:
 	case TLBI_vmalle1:
+	case TLBI_alle1:
+	case TLBI_alle2:
 		return false;
 
 	default:
@@ -219,6 +225,8 @@ enum sm_tlbi_op_stage decode_tlbi_stage_kind(enum tlbi_kind k)
 	case TLBI_vae2is:
 	case TLBI_vmalle1is:
 	case TLBI_vmalle1:
+	case TLBI_alle2:
+	case TLBI_alle2is:
 		return TLBI_OP_STAGE1;
 
 	case TLBI_ipas2e1is:
@@ -226,6 +234,7 @@ enum sm_tlbi_op_stage decode_tlbi_stage_kind(enum tlbi_kind k)
 
 	case TLBI_vmalls12e1:
 	case TLBI_vmalls12e1is:
+	case TLBI_alle1:
 	case TLBI_alle1is:
 		return TLBI_OP_BOTH_STAGES;
 
@@ -239,6 +248,8 @@ enum sm_tlbi_op_regime_kind decode_tlbi_regime_kind(enum tlbi_kind k)
 	switch (k) {
 	case TLBI_vale2is:
 	case TLBI_vae2is:
+	case TLBI_alle2:
+	case TLBI_alle2is:
 		return TLBI_REGIME_EL2;
 
 	case TLBI_vmalle1is:
@@ -246,6 +257,7 @@ enum sm_tlbi_op_regime_kind decode_tlbi_regime_kind(enum tlbi_kind k)
 	case TLBI_ipas2e1is:
 	case TLBI_vmalls12e1:
 	case TLBI_vmalls12e1is:
+	case TLBI_alle1:
 	case TLBI_alle1is:
 		return TLBI_REGIME_EL10;
 
@@ -267,7 +279,10 @@ static bool __decoded_tlbi_has_asid(struct trans_tlbi_data data, u8 *out_asid)
 	case TLBI_ipas2e1is:
 	case TLBI_vmalls12e1:
 	case TLBI_vmalls12e1is:
+	case TLBI_alle1:
 	case TLBI_alle1is:
+	case TLBI_alle2:
+	case TLBI_alle2is:
 		return false;
 
 	default:
