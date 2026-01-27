6401a30b11047277fa40c457d5eec7e3418d47b0
Ben Simner <ben.simner@cl.cam.ac.uk>
2026-01-27 04:56:25 +0000

    (casemate-check-c) two-level quiet
    passing -q disables printing but still prints error messages
    passing -qq suppresses even those

--- src/casemate-check-c/include/casemate-check-impl.h
+++ src/casemate-check-c/include/casemate-check-impl.h
@@ -15,7 +15,7 @@ extern bool SHOULD_TRACE;
 extern bool SHOULD_TRACE_CONDENSED;
 extern bool HARDEN;
 extern bool NO_DEFAULT_SYSREGS;
-extern bool QUIET;
+extern int QUIET;
 extern bool COLOR;
 extern bool DEBUG;
 
