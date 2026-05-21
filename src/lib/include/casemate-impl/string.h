a372bd2ce99398ec296e6d4df1031d811b5e58fd
Ben Simner <benjsimner@gmail.com>
2026-05-21 11:16:47 +0100

    (lib/string) fix: stdlib-compatible prototypes

--- src/lib/include/casemate-impl/string.h
+++ src/lib/include/casemate-impl/string.h
@@ -3,8 +3,10 @@
 
 #include <casemate-impl/types.h>
 
-u64 strlen(const char *s);
+unsigned long strlen(const char *s);
+
 bool streq(const char *s1, const char *s2);
-void *memset(void *ptr, int value, u64 num);
+
+void *memset(void *ptr, int value, unsigned long num);
 
 #endif /* CASEMATE_STRING_H */
