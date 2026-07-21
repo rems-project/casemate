40b99918cd33a03f237d802a080aac5bd56dbd8b
Ben Simner <benjsimner@gmail.com>
2026-07-21 10:44:40 +0100

    (casemate-check-c) fix: no auto-promote TLBIs

--- src/casemate-check-c/src/driver.c
+++ src/casemate-check-c/src/driver.c
@@ -102,8 +102,6 @@ void *initialise_casemate(void)
 		opts.check_opts.print_opts |= CM_PRINT_WHOLE_STATE_ON_STEP;
 	opts.log_opts.condensed_format = SHOULD_TRACE_CONDENSED;
 
-	/* TODO: for now ... */
-	opts.check_opts.promote_TLBI_by_id = true;
 	opts.check_opts.check_synchronisation = SHOULD_CHECK_LOCKS;
 
 	opts.enable_tracing = SHOULD_TRACE;
